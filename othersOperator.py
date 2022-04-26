7048856f584e28c0db1aac4be2df238f6d5ed52a
Kuzey Demir Kurt <104431329+KDKurt097@users.noreply.github.com>
2022-04-27 00:30:29 +0300

    yeni çalışma

---
+++ othersOperator.py
@@ -0,0 +1,15 @@
+# İdentity Operator: is
+# is adresleri karşılaştırır (not kullanarak olumsuz olumsuz yapılabilir)
+
+x=y=[1,2,3]
+z=[1,2,3]
+print(x is y)
+print(x is z)
+
+# Membership Operator: in
+# in içinde var mı anlamındadır ( not kullanılarak olumsuz yapılabilir)
+x = ["elma","muz"]
+print("muz" in x)
+
+
+
